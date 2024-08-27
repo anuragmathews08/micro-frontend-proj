a30dccb74b8ce79e3b78f40ba0b14d82b4d7087e
Anurag Martin Mathews <anurag.mathews@valtech.com>
2024-08-27 10:58:39 +0530

    Inital MFP commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => {
+    return <>
+        <h1>Hi there!</h1>
+        <MarketingApp />
+    </>;
+};
+
+export default App;
